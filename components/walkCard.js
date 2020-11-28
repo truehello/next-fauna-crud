8d6e34a74f1494dfe718102bad759527fb865409
truehello <trudelmarc@gmail.com>
2020-11-27 20:46:25 -0800

    refactored walkCard for home page

---
+++ components/walkCard.js
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import TextTruncate from "react-text-truncate";
+import YouTube from "react-youtube";
+
+const walkCard = ({ data }) => {
+
+  //console.log("in walkcard")
+
+  const opts = {
+    height: "100%",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+
+    return (
+        <li
+              key={data._id}
+              className="flex flex-col justify-between p-4 bg-opacity-75 bg-gradient-to-r from-purple-700 to-blue-500 text-gray-100 rounded"
+            >
+              <div>
+                <YouTube videoId={data.video_id} opts={opts} />
+
+                <p className="mt-4 text-lg font-semibold">{data.name}</p>
+                {/* <p>location: {data.location}</p> */}
+                <div className="flex justify-between pt-2 pb-4">
+                  <p className="text-sm">{data.date}</p>
+                  <p className="text-sm">
+                    {data.city}, {data.country}
+                  </p>
+                </div>
+
+                <TextTruncate
+                  line={3}
+                  element="p"
+                  truncateText="…"
+                  text={data.description}
+                  className="text-gray-200 tracking-tight pt-2"
+                />
+              </div>
+              <div className="flex justify-end pt-4">
+                <Link href="/walk/[id]" as={`/walk/${data._id}`}>
+                  <a className="px-8 py-2 text-lg font-semibold text-white rounded-lg bg-gradient-to-r hover:from-teal-400 hover:to-blue-500 from-pink-600 to-orange-500">
+                    Visit
+                  </a>
+                </Link>
+              </div>
+            </li>
+    )
+}
+
+export default walkCard
--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,9 @@ import { getAuthCookie } from "../utils/auth-cookies";
 import TextTruncate from "react-text-truncate";
 import YouTube from "react-youtube";
 
-const opts = {
-  height: "100%",
-  width: "100%",
-  playerVars: {
-    // https://developers.google.com/youtube/player_parameters
-    autoplay: 0,
-  },
-};
+import WalkCard from "../components/walkCard";
+
+
 
 const Home = ({ token }) => {
   const fetcher = async (query) => await graphQLClient(token).request(query);
@@ -48,49 +43,7 @@ const Home = ({ token }) => {
     fetcher
   );
 
-  // const toggleTodo = async (id, completed) => {
-  //   const mutation = gql`
-  //     mutation PartialUpdateTodo($id: ID!, $completed: Boolean!) {
-  //       partialUpdateTodo(id: $id, data: { completed: $completed }) {
-  //         _id
-  //         completed
-  //       }
-  //     }
-  //   `;
 
-  //   const variables = {
-  //     id,
-  //     completed: !completed,
-  //   };
-
-  //   try {
-  //     await graphQLClient(token)
-  //       .setHeader("X-Schema-Preview", "partial-update-mutation")
-  //       .request(mutation, variables);
-  //     mutate();
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // const deleteATodo = async (id) => {
-  //   const mutation = gql`
-  //     mutation DeleteATodo($id: ID!) {
-  //       deleteTodo(id: $id) {
-  //         _id
-  //       }
-  //     }
-  //   `;
-
-  //   try {
-  //     await graphQLClient(token).request(mutation, { id });
-  //     mutate();
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  //console.log(JSON.stringify(data, null, 2))
 
   if (error)
     return (
@@ -114,38 +67,8 @@ const Home = ({ token }) => {
       {data ? (
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
           {data.allVideos.data.map((video) => (
-            <li
-              key={video._id}
-              className="flex flex-col justify-between p-4 bg-opacity-75 bg-gradient-to-r from-purple-700 to-blue-500 text-gray-100 rounded"
-            >
-              <div>
-                <YouTube videoId={video.video_id} opts={opts} />
-
-                <p className="mt-4 text-lg font-semibold">{video.name}</p>
-                {/* <p>location: {video.location}</p> */}
-                <div className="flex justify-between pt-2 pb-4">
-                  <p className="text-sm">{video.date}</p>
-                  <p className="text-sm">
-                    {video.city}, {video.country}
-                  </p>
-                </div>
-
-                <TextTruncate
-                  line={3}
-                  element="p"
-                  truncateText="…"
-                  text={video.description}
-                  className="text-gray-200 tracking-tight pt-2"
-                />
-              </div>
-              <div className="flex justify-end pt-4">
-                <Link href="/walk/[id]" as={`/walk/${video._id}`}>
-                  <a className="px-8 py-2 text-lg font-semibold text-white rounded-lg bg-gradient-to-r hover:from-teal-400 hover:to-blue-500 from-pink-600 to-orange-500">
-                    Visit
-                  </a>
-                </Link>
-              </div>
-            </li>
+            <WalkCard data={video} key={video._id} />
+           
           ))}
         </ul>
       ) : (
@@ -154,84 +77,7 @@ const Home = ({ token }) => {
     </Layout>
   );
 };
-// return (
-//   <Layout>
-//     <h1 className="text-2xl py-4 mb-4 font-semibold">ToDo WOOHOO</h1>
 
-//     <Link href="/new">
-//       <a className="rounded-md py-2 px-4 text-gray-100 bg-green-500 hover:bg-green-600 focus:outline-none">
-//         Create New Todo
-//       </a>
-//     </Link>
-
-//     <Link href="/addVideo">
-//       <a className="rounded-md py-2 px-4 text-yellow-100 bg-yellow-500 hover:bg-yellow-600 focus:outline-none">
-//         Add a Video
-//       </a>
-//     </Link>
-
-//     {data ? (
-//       <ul className="my-4">
-//         {data.allVideos.data.map((todo) => (
-//           <li
-//             key={video._id}
-//             className="flex items-center justify-between py-4"
-//           >
-//             <p>{video.name}</p>
-//             <p>{video.location}</p>
-
-//             {user && user.id === todo.owner._id ? (
-//                <>
-//                <span
-//                className="text-base font-medium text-gray-700 cursor-pointer"
-//                onClick={() => toggleTodo(todo._id, todo.completed)}
-//                style={
-//                  todo.completed
-//                    ? { textDecorationLine: "line-through" }
-//                    : { textDecorationLine: "none" }
-//                }
-//              >
-//                {todo.task}
-//              </span>
-
-//               <div>
-//                 <span className="ml-4">
-//                   <Link href="/todo/[id]" as={`/todo/${todo._id}`}>
-//                     <a className="rounded-md py-2 px-4 text-gray-100 bg-blue-500 hover:bg-blue-600 focus:outline-none">
-//                       Edit
-//                     </a>
-//                   </Link>
-//                 </span>
-//                 <button
-//                   onClick={() => deleteATodo(todo._id)}
-//                   className=" ml-2 rounded-md py-2 px-4 text-gray-100 bg-red-500 hover:bg-red-600 focus:outline-none"
-//                 >
-//                   Delete
-//                 </button>
-//               </div>
-//               </>
-//             ) : (
-//               <>
-//               <span className="text-base font-medium text-gray-700">
-//               {todo.task}
-//             </span>
-//               <Link href="/todo/[id]" as={`/todo/${todo._id}`}>
-//                 <a className="rounded-md py-2 px-4 text-gray-100 bg-blue-500 hover:bg-blue-600 focus:outline-none">
-//                   View
-//                 </a>
-//               </Link>
-//               </>
-//             )}
-
-//           </li>
-//         ))}
-//       </ul>
-//     ) : (
-//       <div>loading...</div>
-//     )}
-//   </Layout>
-// );
-//};
 
 export async function getServerSideProps(ctx) {
   const token = getAuthCookie(ctx.req);
